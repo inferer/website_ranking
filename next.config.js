dde80405752b232b0bfc45566a525be6dc03888e
token.ens <token.ens@gmail.com>
2023-07-25 10:48:35 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,11 +12,13 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: isDev ? `http://49.234.51.249:8792/api/:path*` : `https://api7.inferer.xyz/api/:path*`
+        destination: `http://49.234.51.249:8792/api/:path*`
+        // destination: isDev ? `http://49.234.51.249:8792/api/:path*` : `https://api7.inferer.xyz/api/:path*`
       },
       {
         source: '/plugin/:path*',
-        destination: isDev ? `http://49.234.51.249:8792/plugin/:path*` : `https://api7.inferer.xyz/plugin/:path*`
+        destination: `http://49.234.51.249:8792/plugin/:path*`
+        // destination: isDev ? `http://49.234.51.249:8792/plugin/:path*` : `https://api7.inferer.xyz/plugin/:path*`
       },
     ]
   },
